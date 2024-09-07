cef392bab221a94cfc1c27678c08d41daeca95dd
Brandon Mehelal <brandon.mehelal@gmail.com>
2024-09-07 13:20:40 -0400

    Changed to post request

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get('/api/v1/elt/{video_id}')
+@app.post('/api/v1/elt/{video_id}')
 def elt(video_id: str, db: Session = Depends(get_db), status_code=200):
     try:
         comments = extract_elt(video_id)
