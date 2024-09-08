ff62d87e987f1f2b510a1e6519c631f054672c1b
Brandon Mehelal <brandon.mehelal@gmail.com>
2024-09-07 21:51:31 -0400

    Changes to databasse

--- database/models.py
+++ database/models.py
@@ -5,8 +5,9 @@ from database.database import Base
 class Comments(Base):
     __tablename__ = 'comments'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)  
-    author = Column(String(255), nullable=False)  
-    author_image_url = Column(String, nullable=True) 
-    content = Column(Text, nullable=False)  
-    date = Column(Date, nullable=False)  
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    author = Column(String(255), nullable=False)
+    author_image_url = Column(String, nullable=True)
+    content = Column(Text, nullable=False)
+    date = Column(Date, nullable=False)
+    sentiment = Column(String, nullable=True, default=None)
